86a25f4b6d8f133984224c5b9e5d989b983147b1
flowside <plowsidee@gmail.com>
2023-09-28 21:48:53 +0700

    add tests

---
+++ C#/C#/test.cs
@@ -0,0 +1,54 @@
+tests exec = new tests();
+exec.test_fibonacci();
+
+
+class tests
+{
+    public int get_fib_recursive(int _)
+    {
+        if (_ <= 1) return _;
+        else return get_fib_recursive(_ - 1) + get_fib_recursive(_ - 2);
+    }
+
+    public int get_fib(int v)
+    {
+        if (v <= 1) return v;
+
+        int _ = 0; int __ = 1; int res = 0;
+
+        for (int i = 2; i <= v; i++)
+        {
+            res = _ + __;
+            _ = __;
+            __ = res;
+        }
+
+        return res;
+    }
+
+    public void test_fibonacci()
+    {
+        Console.Write("Введите sdfчисло: ");
+        int v; int _ = 0; DateTime start;
+        while (true)
+        {
+            try { v = int.Parse(Console.ReadLine()); break; }
+            catch (Exception e) { Console.WriteLine(e); }
+        }
+        Console.WriteLine($"Числа фибоначчи от 0 до {v}:");
+        start = DateTime.Now;
+        for (int i = 0; i <= v; i++)
+        {
+            _ = get_fib(i);
+        }
+        Console.WriteLine($"Время на обработку циклом: {DateTime.Now - start} | Результат: {_}");
+
+        start = DateTime.Now;
+        for (int i = 0; i <= v; i++)
+        {
+            _ = get_fib_recursive(i);
+        }
+        Console.WriteLine($"Время на обработку рекурсией: {DateTime.Now - start} | Результат: {_}");
+    }
+
+}
